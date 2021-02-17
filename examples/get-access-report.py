355f2cf8226ef8b42610e3813b77bbdfac2472ec
Cloud User <centos@privx.ukmdemo.com>
2021-02-17 18:33:48 +0000

    Changed script logic and added offset and limit attributes

--- examples/get-access-report.py
+++ examples/get-access-report.py
@@ -17,74 +17,57 @@ api = privx_api.PrivXAPI(config.HOSTNAME, config.HOSTPORT, config.CA_CERT,
 # NOTE: fill in your credentials from secure storage, this is just an example
 api.authenticate("API client ID", "API client secret")
 
-
-def get_roles():
-    resp = api.get_roles()
+def get_connection_data(user):
+    offset = 0
+    limit = 1000
+    resp = api.search_connections(offset=offset, limit=limit)
     if resp.ok():
         data_load = resp.data()
-        roles_data = {}
-        for role_data in data_load['items']:
-            name = role_data['name']
-            roles_data[name] = role_data['id']
-        return roles_data
-    else:
-        error = "Get Roles operation failed:"
-        process_error(error)
-
-
-def get_users(role_id):
-    resp = api.get_role_members(role_id)
-    if resp.ok():
-        data_load = resp.data()
-        users_data = {}
-        for user_data in data_load['items']:
-            user = user_data['principal']
-            users_data[user] = user_data['id']
-        return users_data
-    else:
-        error = "Get users operation failed:"
-        process_error(error)
-
-
-def get_connection_data(uid, user):
-    resp = api.search_connections(user_id=[uid])
-    if resp.ok():
-        data_load = resp.data()
+        data_items = data_load['items']
+        count = data_load['count'] - limit
+        while count > 0:
+            offset = offset + limit
+            resp = api.search_connections(offset=offset, limit=limit)
+            data_load = resp.data()
+            if resp.ok():
+                data_items = data_items + data_load['items']
+                count = count - limit
         connections_data = {}
         all_data = []
-        connections_data["user"] = user
-        for connection_data in data_load['items']:
-            data = ("type,target_host_address,"
-                    "target_host_account,connected,disconnected")
-            data_list = data.split(',')
-            for p in data_list:
-                if p in ("connected", "disconnected"):
-                    connection_data[p] = connection_data[p].split(".")[0]
-                connections_data[p] = connection_data[p]
-            all_data.append(dict(connections_data))
+        data = ("type,mode,authentication_method,target_host_address,"
+                "target_host_account,connected,disconnected")
+        data_list = data.split(',')
+        for connection_data in data_items:
+            principal = connection_data['user']['display_name']
+            if user in ("ALL", principal):
+                connections_data["user"] = connection_data['user']['display_name']
+                for p in data_list:
+                    if p in ("connected", "disconnected"):
+                        connection_data[p] = connection_data[p].split(".")[0]
+                    if p == "authentication_method":
+                        connection_data[p] = ','.join(connection_data[p])
+                    connections_data[p] = connection_data[p]
+                all_data.append(dict(connections_data))
+        if not len(all_data):
+            print("No connection data found for "+user)
+            sys.exit(2)
         return all_data
     else:
         error = "Get users Connection data operation failed:"
         process_error(error)
 
 
-def export_connection_data(user, users_data, header_printed=False):
+def export_connection_data(user, header_printed=False):
     output_csvfile = user+"_connection_data.csv"
-    users = {}
-    if user == "ALL":
-        users = users_data
-    else:
-        users[user] = users_data[user]
     with open(output_csvfile, "w") as f:
+        connection_data = get_connection_data(user)
         print("Writing Connection data to", output_csvfile, end=' ')
-        for user, uid in users.items():
-            connection_data = get_connection_data(uid, user)
-            for data in connection_data:
-                w = csv.DictWriter(f, data.keys())
-                if not header_printed:
-                    w.writeheader()
-                    header_printed = True
-                w.writerow(data)
+        for data in connection_data:
+            w = csv.DictWriter(f, data.keys())
+            if not header_printed:
+                w.writeheader()
+                header_printed = True
+            w.writerow(data)
     print("\nDone")
 
 
@@ -118,15 +101,7 @@ def main():
             user = arg
         else:
             usage()
-    roles_data = get_roles()
-    role_id = roles_data['privx-user']
-    users_data = get_users(role_id)
-    if (user == "ALL" or user in users_data.keys()):
-        export_connection_data(user, users_data)
-    else:
-        print(user+" user not found")
-        sys.exit(2)
-
+    export_connection_data(user)
 
 if __name__ == '__main__':
     main()
