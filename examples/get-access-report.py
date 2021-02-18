c7b6d8d5059b08630325b7f9a4b6146e6bf467e4
Cloud User <centos@privx.ukmdemo.com>
2021-02-18 13:41:42 +0000

    added search_user to get user_id and extract connections using user_id

--- examples/get-access-report.py
+++ examples/get-access-report.py
@@ -17,17 +17,42 @@ api = privx_api.PrivXAPI(config.HOSTNAME, config.HOSTPORT, config.CA_CERT,
 # NOTE: fill in your credentials from secure storage, this is just an example
 api.authenticate("API client ID", "API client secret")
 
-def get_connection_data(user):
+def get_user_id(user):
+    resp = api.search_users(keywords=user)
+    if resp.ok():
+        data_load = resp.data()
+        data_items = data_load['items']
+        user_id = False
+        for user_data in data_items:
+            if user_data['principal'] == user:
+                user_id = user_data['id']
+        if user_id:
+            return user_id
+        else:
+            print(user+": User not found")
+            sys.exit(2)
+    else:
+        error = "Get users operation failed:"
+        process_error(error)
+
+def get_connection_data(user_id):
     offset = 0
     limit = 1000
-    resp = api.search_connections(offset=offset, limit=limit)
+    if user_id:
+        resp = api.search_connections(user_id=[user_id], offset=offset, limit=limit)
+    else:
+        resp = api.search_connections(offset=offset, limit=limit)
+
     if resp.ok():
         data_load = resp.data()
         data_items = data_load['items']
         count = data_load['count'] - limit
         while count > 0:
             offset = offset + limit
-            resp = api.search_connections(offset=offset, limit=limit)
+            if user_id:
+                resp = api.search_connections(user_id=[user_id], offset=offset, limit=limit)
+            else:
+                resp = api.search_connections(offset=offset, limit=limit)
             data_load = resp.data()
             if resp.ok():
                 data_items = data_items + data_load['items']
@@ -38,29 +63,24 @@ def get_connection_data(user):
                 "target_host_account,connected,disconnected")
         data_list = data.split(',')
         for connection_data in data_items:
-            principal = connection_data['user']['display_name']
-            if user in ("ALL", principal):
-                connections_data["user"] = connection_data['user']['display_name']
-                for p in data_list:
-                    if p in ("connected", "disconnected"):
-                        connection_data[p] = connection_data[p].split(".")[0]
-                    if p == "authentication_method":
-                        connection_data[p] = ','.join(connection_data[p])
-                    connections_data[p] = connection_data[p]
-                all_data.append(dict(connections_data))
-        if not len(all_data):
-            print("No connection data found for "+user)
-            sys.exit(2)
+            connections_data["user"] = connection_data['user']['display_name']
+            for p in data_list:
+                if p in ("connected", "disconnected"):
+                    connection_data[p] = connection_data[p].split(".")[0]
+                if p == "authentication_method":
+                    connection_data[p] = ','.join(connection_data[p])
+                connections_data[p] = connection_data[p]
+            all_data.append(dict(connections_data))
         return all_data
     else:
         error = "Get users Connection data operation failed:"
         process_error(error)
 
 
-def export_connection_data(user, header_printed=False):
+def export_connection_data(user, user_id=None, header_printed=False):
     output_csvfile = user+"_connection_data.csv"
     with open(output_csvfile, "w") as f:
-        connection_data = get_connection_data(user)
+        connection_data = get_connection_data(user_id)
         print("Writing Connection data to", output_csvfile, end=' ')
         for data in connection_data:
             w = csv.DictWriter(f, data.keys())
@@ -101,7 +121,12 @@ def main():
             user = arg
         else:
             usage()
-    export_connection_data(user)
+    if user == "ALL":
+        export_connection_data(user)
+    else:
+        user_id = get_user_id(user)
+        export_connection_data(user, user_id)
+
 
 if __name__ == '__main__':
     main()
