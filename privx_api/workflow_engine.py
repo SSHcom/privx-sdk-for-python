3716e79c18b43b64d42a6c37fc37d7c8ab56c000
Hani Aloui <hani.aloui@ssh.com>
2021-12-09 15:39:32 +0200

    extending workflow-engine class with new revoke_request_target_role method

--- privx_api/workflow_engine.py
+++ privx_api/workflow_engine.py
@@ -178,6 +178,19 @@ class WorkFlowEngineAPI(BasePrivXAPI):
         )
         return PrivXAPIResponse(response_status, HTTPStatus.OK, data)
 
+    def revoke_request_target_role(self, request_id: str) -> PrivXAPIResponse:
+        """
+        Revoke the target role of a request.
+        Only one of the original approvers can revoke the target role.
+
+        Returns:
+            PrivXAPIResponse
+        """
+        response_status, data = self._http_post(
+            UrlEnum.WORKFLOW_ENGINE.REQUEST_ROLE_REVOKE,
+            path_params={"request_id": request_id},
+            )
+
     def search_requests(
         self,
         filter_param: str,
