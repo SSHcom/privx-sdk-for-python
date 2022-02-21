6c01bf9de2cf1d0effe039db2c67dd7cf01fc744
kushns <kushns@users.noreply.github.com>
2022-02-21 15:56:58 +0000

    Added fix for Incorrect padding

--- examples/get-cert.py
+++ examples/get-cert.py
@@ -67,7 +67,7 @@ def get_cert(target_host_config):
         # Fix for Incorrect padding
         cert_data_fix = cert_data + ('=' * ((4 - len(cert_data) % 4) % 4))
 
-        cert_str = " ".join([CERT_PREFIX, cert_data])
+        cert_str = " ".join([CERT_PREFIX, cert_data_fix])
         return cert_str
     else:
         print(cert.data.get("details"))
