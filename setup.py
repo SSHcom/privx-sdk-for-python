4081a42d57735d00be6d8bdb2476f33bbea413cc
Janne Heinonen <janne.heinonen@ssh.com>
2023-03-21 14:17:30 +0200

    Added correct version number and licensing information

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@ from setuptools import setup
 
 setup(
     name='privx_api',
-    version='0.1',
+    version='26.0.0',
     packages=['privx_api'],
+    license='Apache Licence 2.0',
+    url='https://github.com/SSHcom/privx-sdk-for-python',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+	'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
+
