356c1175243cf7df893aea91bf5236ce39f918fd
jmo <jiehong.mo@ssh.com>
2023-08-18 16:29:24 +0300

    ADD access group CA key APIs

---
+++ examples/example-access-group-key.py
@@ -0,0 +1,62 @@
+# An example how to create a CA key for a access group and delete it.
+# Import the PrivX python library.
+import sys
+
+import config
+
+# this importation for demonstration purpose only
+# for proper importation of privx_api module
+# see https://github.com/SSHcom/privx-sdk-for-python#getting-started
+try:
+    # Running example with pip-installed SDK
+    import privx_api
+except ImportError:
+    # Running example without installing SDK
+    from utils import load_privx_api_lib_path
+
+    load_privx_api_lib_path()
+    import privx_api
+
+# eplace with the params of the access group ID and ca key ID
+ACCESS_GROUP_ID = "ACCESS_GROUP_ID"
+CA_ID = "CA_ID"
+
+# Initialize the API.
+api = privx_api.PrivXAPI(
+    config.HOSTNAME,
+    config.HOSTPORT,
+    config.CA_CERT,
+    config.OAUTH_CLIENT_ID,
+    config.OAUTH_CLIENT_SECRET,
+)
+
+# Authenticate.
+# NOTE: fill in your credentials from secure storage, this is just an example
+api.authenticate(config.API_CLIENT_ID, config.API_CLIENT_SECRET)
+
+def get_access_group_CA_key(access_group_id):
+    result = api.get_access_group_CA_key(access_group_id)
+    if result.ok:
+        return result.data
+    else:
+        print(result.data.get("details"))
+        sys.exit()
+
+
+def delete_access_group_CA_key(access_group_id, ca_id):
+    result = api.delete_access_group_CA_key(access_group_id, ca_id)
+    if result.ok:
+        print(result)
+        return
+    else:
+        print(result.data.get("details"))
+        sys.exit()
+
+
+def main():
+    ca_id = get_access_group_CA_key(ACCESS_GROUP_ID)
+    print(ca_id)
+    delete_access_group_CA_key(ACCESS_GROUP_ID, ca_id)
+
+if __name__ == "__main__":
+    main()
--- privx_api/enums.py
+++ privx_api/enums.py
@@ -374,6 +374,8 @@ class TrailIndexEnum:
 class AuthorizerEnum:
     ACCESS_GROUP = "AUTHORIZER.ACCESS_GROUP"
     ACCESS_GROUPS = "AUTHORIZER.ACCESS_GROUPS"
+    CREATE_ACCESS_GROUP_CA_KEY = "AUTHORIZER.CREATE_ACCESS_GROUP_CA_KEY"
+    DELETE_ACCESS_GROUP_CA_KEY = "AUTHORIZER.DELETE_ACCESS_GROUP_CA_KEY"
     AUTHORIZER_CERT = "AUTHORIZER.AUTHORIZER_CERT"
     AUTHORIZER_CERT_ID = "AUTHORIZER.AUTHORIZER_CERT_ID"
     CARRIER_CONFIG_SESSION_ID = "AUTHORIZER.CARRIER_CONFIG_SESSION_ID"
@@ -407,6 +409,8 @@ class AuthorizerEnum:
     urls = {
         ACCESS_GROUP: "/authorizer/api/v1/accessgroups/{id}",
         ACCESS_GROUPS: "/authorizer/api/v1/accessgroups",
+        CREATE_ACCESS_GROUP_CA_KEY: "/authorizer/api/v1/accessgroups/{id}/cas",
+        DELETE_ACCESS_GROUP_CA_KEY: "/authorizer/api/v1/accessgroups/{id}/cas/{ca_id}",
         AUTHORIZER_CERT: "/authorizer/api/v1/cas",
         AUTHORIZER_CERT_ID: "/authorizer/api/v1/cas/{id}",
         CARRIER_CONFIG_SESSION_ID: "/authorizer/api/v1/carrier/conf"
