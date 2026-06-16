5fc71d19f87803c1487935a01dde0dea9a0956b3
RomanSemkin <boogaloosurf@gmail.com>
2026-06-16 11:37:50 +0300

    add(conn-mngr) new query params for search/get connections

--- privx_api/connection_manager.py
+++ privx_api/connection_manager.py
@@ -26,6 +26,7 @@ class ConnectionManagerAPI(BasePrivXAPI):
         sort_dir: Optional[str] = None,
         fuzzy_count: Optional[bool] = False,
         verbose: Optional[bool] = False,
+        item_format: Optional[str] = None,
     ) -> PrivXAPIResponse:
         """
         Get connections.
@@ -40,6 +41,7 @@ class ConnectionManagerAPI(BasePrivXAPI):
             sortdir=sort_dir,
             fuzzycount=bool(fuzzy_count),
             verbose=bool(verbose),
+            item_format=item_format,
         )
         response_status, data = self._http_get(
             UrlEnum.CONNECTION_MANAGER.CONNECTIONS,
@@ -56,6 +58,7 @@ class ConnectionManagerAPI(BasePrivXAPI):
         connection_params: Optional[dict] = None,
         fuzzy_count: Optional[bool] = False,
         verbose: Optional[bool] = False,
+        item_format: Optional[str] = None,
     ) -> PrivXAPIResponse:
         """
         Search for connections.
@@ -70,6 +73,7 @@ class ConnectionManagerAPI(BasePrivXAPI):
             sortdir=sort_dir,
             fuzzycount=bool(fuzzy_count),
             verbose=bool(verbose),
+            item_format=item_format,
         )
 
         response_status, data = self._http_post(
